7314cba3c6033f5ab9a80586cfb39de6320c444a
ArtSatanov <artsatanov@gmail.com>
2023-12-18 02:47:07 +0200

    1

--- index.js
+++ index.js
@@ -1,18 +1,34 @@
 import * as contactsService from './contacts.js';
+import { program } from 'commander';
 
-invokeContacts = async ({ action, id, name, email, phone }) => {
+const invokeAction = async ({ action, id, name, email, phone }) => {
   switch (action) {
     case 'list':
       const allContacts = await contactsService.listContacts();
-      return console.log(allContacts);
+      return console.table(allContacts);
     case 'getById':
       const oneContact = await contactsService.removeContact(id);
-      return oneContact;
+      return console.log(oneContact);
     case 'addContact':
       const addContact = await contactsService.addContact(name, email, phone);
-      return addContact;
+      return console.log(addContact);
     case 'removeContact':
       const removeContact = await contactsService.removeContact(id);
-      return removeContact;
+      return console.log(removeContact);
+    default:
+      console.warn('\x1B[31m Unknown action type!');
   }
 };
+
+program
+  .option('-a, --action <type>', 'choose action')
+  .option('-i, --id <type>', 'user id')
+  .option('-n, --name <type>', 'user name')
+  .option('-e, --email <type>', 'user email')
+  .option('-p, --phone <type>', 'user phone');
+
+program.parse();
+
+const argv = program.opts();
+
+invokeAction(argv);
